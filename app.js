90cec67a8214d10c303481b42a2c713c68d7c5f0
sleelily <sleelily@live.com>
2019-02-24 11:55:14 +0800

    增加基础爬虫支持

--- app.js
+++ app.js
@@ -1,44 +1,100 @@
-var express = require('express');
-var cheerio = require('cheerio');
-var ep = require('eventproxy');
-var async = require('async');
-var superagent = require('superagent');
+const cheerio = require('cheerio');
+const superagent = require('superagent');
+const config = require('config');
+const groupName = config.groupName || 'beijingzufang';
+const moment = require('moment');
 
-var app = express();
+const PERSON_REGEXP = /people\/(\w+)\//;
+const TOPIC_REGEXP = /topic\/(\d+)\//;
+const MONTH_AND_DATE = /^\d{2}-\d{2}\s\d{2}:\d{2}$/;
+const YEAR_AND_MONTH = /^\d{4}-\d{2}-\d{2}$/;
+const YEAR_STRING = moment().format('YYYY-');
 
-app.set('views','views');
-app.set('view engine','jade');
+async function main() {
+  const URL_MAIN = `http://www.douban.com/group/${groupName}/discussion`
+  
+  try {
+    const res = await superagent.get(URL_MAIN);
 
-app.get('/',function(req, res, next){
-  superagent.get('http://daily.zhihu.com/')
-  .end(function(err,sres){
-    if(err){
-      console.error(err);
-    }
+    // 处理正文
+    const $ = cheerio.load(res.text);
+    const items = [];
+    
+    // 收集简略信息
+    $('.article table tr:not(.th)').each(function(_, elem){
+      const $element = $(elem);
+      const timeString = $element.children("td:last-child").text();
 
-    var $ = cheerio.load(sres.text);
-    var items = [];
-    $('.box a').each(function(idx,element){
-      var $element = $(element);
+      let updateTime;
+      if (MONTH_AND_DATE.test(timeString)) {
+        updateTime = moment(`${YEAR_STRING}${timeString}`).valueOf();
+      } else if (YEAR_AND_MONTH.test(timeString)){
+        updateTime =  moment(`${timeString}`).valueOf();
+      } else {
+        console.log('该日期格式无法解析 :: ', timeString);
+      }
+
       items.push({
-        title:$element.children(".title").text(),
-        premage:$element.children(".preview-image").attr('src'),
-        href:$element.attr('href')
+        title: $element.children("td.title").children('a').attr('title'), 
+        id: TOPIC_REGEXP.exec($element.children("td.title").children('a').attr('href'))[1],
+        people: {
+          name: $element.children("td:nth-child(2)").text(),
+          id: PERSON_REGEXP.exec($element.children("td:nth-child(2)").children('a').attr('href'))[1],
+        },
+        repeat: +$element.children("td:nth-child(3)").text(),
+        updateTime
       });
     });
 
-    items.forEach(function(item){
-      console.log(item);
+    console.log('items:: ', items);
+  } catch (error) {
+    console.log('出现错误 :: ', error);
+  }
+  
+  process.exit();
+};
+
+// main();
+
+async function detail(topic) {
+  const DETAIL_URL = getTopicUrl(topic.id);
+
+  try {
+    const res = await superagent.get(DETAIL_URL);
+
+    // 处理正文
+    const $ = cheerio.load(res.text);
+    // 创建时间
+    const createdTime = $('.topic-doc h3 span:nth-child(2)').text();
+    // 正文内容
+    const topicContent = $('.topic-content p').text();
+    // 收集正文图片地址
+    const topicImages = [];
+    $('.topic-content image-container imp').each((_, elem) => {
+      const $image = $(elem);
+      topicImages.push($image.attr('src'));
     });
 
-    res.render('index',{
-      title : '知乎日报爬虫',
-      items : items
-    });
-  });
+    const content = {
+      createdTime,
+      topicContent,
+      topicImages,
+    }
+
+    console.log('content %j', content);
+  } catch (error) {
+    console.log('解析topic内容失败 :: ', error);
+  }
+}
+
+detail({
+  title: '北京市青年社区33个社区出租，只为给您一个家～非中介',
+  id: '132795683',
+  people: { name: '百晓生', id: '149857351' },
+  repeat: '135',
+  updateTime: 1550912940000
 });
 
-
-
-app.listen(3000);
-
+function getTopicUrl (id) {
+  return `http://www.douban.com/group/topic/${id}/`;
+}
