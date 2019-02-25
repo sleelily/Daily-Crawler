598bdd01076bf547603aba78ff5aedd6c822f4c1
sleelily <sleelily@live.com>
2019-02-25 16:49:57 +0800

    增加分词支持及自定义字典

--- app.js
+++ app.js
@@ -10,6 +10,12 @@ const MONTH_AND_DATE = /^\d{2}-\d{2}\s\d{2}:\d{2}$/;
 const YEAR_AND_MONTH = /^\d{4}-\d{2}-\d{2}$/;
 const YEAR_STRING = moment().format('YYYY-');
 
+const nodejieba = require("nodejieba");
+
+nodejieba.load({
+  userDict: './dict/user.dict.utf8',
+});
+
 async function main() {
   const URL_MAIN = `http://www.douban.com/group/${groupName}/discussion`
   
@@ -87,14 +93,21 @@ async function detail(topic) {
   }
 }
 
-detail({
-  title: '北京市青年社区33个社区出租，只为给您一个家～非中介',
-  id: '132795683',
-  people: { name: '百晓生', id: '149857351' },
-  repeat: '135',
-  updateTime: 1550912940000
-});
+// detail({
+//   title: '北京市青年社区33个社区出租，只为给您一个家～非中介',
+//   id: '132795683',
+//   people: { name: '百晓生', id: '149857351' },
+//   repeat: '135',
+//   updateTime: 1550912940000
+// });
 
 function getTopicUrl (id) {
   return `http://www.douban.com/group/topic/${id}/`;
 }
+
+
+var result = nodejieba.tag("小区位于地铁六号线物资学院路东北口附近 小区附近设有多路公交站 交通方便 小区离地铁口近 小区内有大型华联超市 房间为三家合租 朝南正规次卧 干净整洁 包物业取暖可做饭 可压一付一 可短租 随时看房 拎包入住 联系电话13401099726");
+console.log(result);
+
+
+// http://www.douban.com/group/topic/132795683
